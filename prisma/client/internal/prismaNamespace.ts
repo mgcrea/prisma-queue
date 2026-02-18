b31ae36559351341601a008e0677ddd1c74ea0f3
Andreas Korth <andreas.korth@gmail.com>
2026-02-18 23:28:42 +0100

    chore: update to prisma 7.4.0

--- prisma/client/internal/prismaNamespace.ts
+++ prisma/client/internal/prismaNamespace.ts
@@ -80,12 +80,12 @@ export type PrismaVersion = {
 }
 
 /**
- * Prisma Client JS version: 7.3.0
- * Query Engine version: 9d6ad21cbbceab97458517b147a6a09ff43aa735
+ * Prisma Client JS version: 7.4.0
+ * Query Engine version: ab56fe763f921d033a6c195e7ddeb3e255bdbb57
  */
 export const prismaVersion: PrismaVersion = {
-  client: "7.3.0",
-  engine: "9d6ad21cbbceab97458517b147a6a09ff43aa735"
+  client: "7.4.0",
+  engine: "ab56fe763f921d033a6c195e7ddeb3e255bdbb57"
 }
 
 /**
